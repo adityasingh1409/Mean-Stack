d5702e0e5a4c96d77e29025ff2a4e4441a4c39b1
Aditya <adityasingh233@lpu.in>
2026-07-01 10:45:06 +0530

    Enhance user interface and data structure by updating User model, adding user data, and improving navbar styles

--- routepractise/src/Utils/utils.ts
+++ routepractise/src/Utils/utils.ts
@@ -1,8 +1,113 @@
 export interface User {
-  name: string; 
   id: number;
+  name: string;
   age: number;
   email: string;
   phone: string;
   city: string;
+  profession: string;
+  salary: number;
 }
+
+export const users: User[] = [
+  {
+    id: 1,
+    name: "Aarav Sharma",
+    age: 22,
+    email: "aarav@example.com",
+    phone: "9876543210",
+    city: "Delhi",
+    profession: "Software Engineer",
+    salary: 65000,
+  },
+  {
+    id: 2,
+    name: "Priya Verma",
+    age: 25,
+    email: "priya@example.com",
+    phone: "9876543211",
+    city: "Mumbai",
+    profession: "UI/UX Designer",
+    salary: 70000,
+  },
+  {
+    id: 3,
+    name: "Rohan Singh",
+    age: 28,
+    email: "rohan@example.com",
+    phone: "9876543212",
+    city: "Noida",
+    profession: "Backend Developer",
+    salary: 85000,
+  },
+  {
+    id: 4,
+    name: "Sneha Patel",
+    age: 24,
+    email: "sneha@example.com",
+    phone: "9876543213",
+    city: "Ahmedabad",
+    profession: "QA Engineer",
+    salary: 60000,
+  },
+  {
+    id: 5,
+    name: "Vikram Mehta",
+    age: 30,
+    email: "vikram@example.com",
+    phone: "9876543214",
+    city: "Pune",
+    profession: "DevOps Engineer",
+    salary: 95000,
+  },
+  {
+    id: 6,
+    name: "Ananya Gupta",
+    age: 21,
+    email: "ananya@example.com",
+    phone: "9876543215",
+    city: "Jaipur",
+    profession: "Frontend Developer",
+    salary: 55000,
+  },
+  {
+    id: 7,
+    name: "Kabir Malhotra",
+    age: 27,
+    email: "kabir@example.com",
+    phone: "9876543216",
+    city: "Chandigarh",
+    profession: "Data Analyst",
+    salary: 78000,
+  },
+  {
+    id: 8,
+    name: "Meera Joshi",
+    age: 26,
+    email: "meera@example.com",
+    phone: "9876543217",
+    city: "Bengaluru",
+    profession: "Product Manager",
+    salary: 110000,
+  },
+  {
+    id: 9,
+    name: "Aditya Kapoor",
+    age: 29,
+    email: "aditya@example.com",
+    phone: "9876543218",
+    city: "Hyderabad",
+    profession: "Full Stack Developer",
+    salary: 98000,
+  },
+  {
+    id: 10,
+    name: "Ishita Roy",
+    age: 23,
+    email: "ishita@example.com",
+    phone: "9876543219",
+    city: "Kolkata",
+    profession: "Mobile Developer",
+    salary: 72000,
+  },
+];
--- routepractise/src/app/pages/home/home.ts
+++ routepractise/src/app/pages/home/home.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { users } from '../../../Utils/utils';
 
 @Component({
   selector: 'app-home',
@@ -7,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './home.css',
 })
 export class Home {
-  
+  users = users;
 }
